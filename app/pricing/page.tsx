94a24ce72c0b6556543b751b36f8e3e8f12e77da
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-31 21:05:04 -0300

    fix page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,18 +1,8 @@
-import { cookies } from "next/headers"
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+'use client'
 
 import { DataTable } from "./data-table"
 
-export default async function Pricing() {
-  const supabase = createRouteHandlerClient({ cookies })
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-  const { data: profile } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("user_id", user?.id)
-    .single()
+export default function Pricing() {
 
   return (
     <div className="flex flex-col space-y-4 px-4 py-6">
